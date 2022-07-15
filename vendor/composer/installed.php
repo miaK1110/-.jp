c5cc59b5e4c2cf42c4a97f00cc9f79f5da7cf38a
Misa <sukithewestie1110@gmail.com>
2022-07-15 14:58:41 +0900

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fe268772a6347f650a6287e3716bd80bda1a5f7a',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fe268772a6347f650a6287e3716bd80bda1a5f7a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
